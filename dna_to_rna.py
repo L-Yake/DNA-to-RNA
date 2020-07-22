207ad9c2b9e9716dba30f631923cb8b7fbd6ea18
Lily <lilycl@protonmail.com>
2020-07-22 10:21:51 -0600

    simplify to one-liner

--- dna_to_rna.py
+++ dna_to_rna.py
@@ -4,30 +4,22 @@ Translate DNA to RNA using Python Strings
 Reference: https://www.codewars.com/kata/5556282156230d0e5e000089/train/python
 """
 
-proteins = 'TTTT'
-protein2 = 'GCAT'
 
 def dna_to_rna(dna):
     """
     DNA is composed of Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T')
     RNA is composed of the same except for Thymine('T') which is replaced by Uracil ('U')
 
-    Example:
-        proteins = 'TTTT'
-        dna_to_rna(proteins)
+    Example 1:
+        dna_proteins = 'TTTT'
+        dna_to_rna(dna_proteins)
+    >>> 'UUUU'
 
-        >>> 'UUUU'
+    Example 2:
+        dna_proteins = 'GCAT'
+        dna_to_rna(dna_proteins)
+    >>> 'GCAU'
     """
-    # Empty RNA string
-    rna = ''
-    # Check each piece of information in DNA for Thymine('T')
-    for piece in dna:
-        # If piece is Thymine('T') convert to Uracil('U')
-        if piece == 'T':
-            # Add piece onto RNA
-            rna += 'U'
-        else:
-            rna += piece
+    return dna.replace('T', 'U')
 
 
-dna_to_rna(protein2)
